83c24fad799898817cf1af28ef2de965e7733529
Vinny Pilone <vinny@pilone.org>
2024-04-22 17:49:06 -0400

    Functional Version, no major bugs

--- FocusPointsAutoNumber/FocusPointsAutoNumber.py
+++ FocusPointsAutoNumber/FocusPointsAutoNumber.py
@@ -1,1 +1,15 @@
 import vs
+
+
+def main():
+    vs.ForEachObject(channelLights, ("(SEL = True) & (PON = 'Lighting Device')"))
+
+
+def channelLights(ptr):
+    focus = vs.LDevice_GetParamStr(ptr, 0, -2, "Focus")
+    chan = ord(focus.lower()) - 96
+    vs.LDevice_SetParamStr(ptr, 0, -2, "Channel", chan + offset)
+
+
+offset = vs.IntDialog("Enter the offset for this system.", 0)
+main()
