490ea39d91db6e0c702f61f9eb55a64f744dacc2
Vinny Pilone <vinny@pilone.org>
2024-04-22 19:20:04 -0400

    Added support for longer names, fixed null error

--- FocusPointsAutoNumber/FocusPointsAutoNumber.py
+++ FocusPointsAutoNumber/FocusPointsAutoNumber.py
@@ -6,9 +6,15 @@ def main():
 
 
 def channelLights(ptr):
-    focus = vs.LDevice_GetParamStr(ptr, 0, -2, "Focus")
-    chan = ord(focus.lower()) - 96
-    vs.LDevice_SetParamStr(ptr, 0, -2, "Channel", chan + offset)
+    focus = vs.LDevice_GetParamStr(ptr, 0, -2, "Focus").strip()
+    if focus.__len__() > 0:
+        chan = 0
+        if focus.__len__() > 1:
+            for i in range(focus.__len__()):
+                chan += (ord(focus[i - 1].lower()) - 96) * (26 ** ((i)))
+        else:
+            chan = ord(focus.lower()) - 96
+        vs.LDevice_SetParamStr(ptr, 0, -2, "Channel", chan + offset)
 
 
 offset = vs.IntDialog("Enter the offset for this system.", 0)
