cf55334b1e9622cc18ccc6470f6b41b076c2efeb
Vinny Pilone <vinny@pilone.org>
2024-09-21 15:46:09 -0400

    Functioning prototype, but only one line at a time

--- Photometrics Help/PhotometricsAutomate.py
+++ Photometrics Help/PhotometricsAutomate.py
@@ -1,35 +1,52 @@
 import vs
-import Vectorworks_Photometrics as VPDialog
+
+# import Vectorworks_Photometrics as VPDialog
 
 # area height in feet
 areaHeight = 6
 isToplight = False
+lineHandles = []
+global activehandle
+
+userPt1 = vs.GetMouse()
+userPt2 = vs.GetMouse()
 
 
 def _setVariables():
     areaHeight = vs.IntDialog("Please enter the height of your area in feet", "6")
-    isToplight = vs.YNDialog("Is this toplight?")
+    # isToplight = vs.YNDialog("Is this toplight?")
 
 
-def _SetupPhotometrics(line):
-    def _DrawPhotometrics(pt1, pt2):
-        vs.MoveTo(pt1)
-        upperPt = pt1
-        upperPt.y += 6
-        vs.LineTo(upperPt)
-        vs.MoveTo(upperPt)
-        vs.LineTo(pt2)
-        drawnLine1 = vs.LNewObj()
-        if pt1.x == vs.GetSegPt1(line).x:
-            vs.MoveTo(vs.GetSegPt2(line))
-        else:
-            vs.MoveTo(vs.GetSegPt1(line))
-        vs.LineTo(pt2)
-        drawnLine2 = vs.LNewObj()
+# called by getline
+def _SavePts(LinePt1, LinePt2):
+    global userPt1
+    userPt1 = LinePt1
+    global userPt2
+    userPt2 = LinePt2
+    _SetupPhotometrics()
 
-    vs.SetSelect(line)
-    vs.GetLine(_DrawPhotometrics)
+
+# called by _savePts
+def _SetupPhotometrics():
+    vs.MoveTo(userPt1)
+    upperPt = (userPt1[0], userPt1[1] + (areaHeight * 12))
+    vs.LineTo(upperPt)
+    vs.MoveTo(upperPt)
+    vs.LineTo(userPt2)
+    endPt = vs.GetMouse()
+    if userPt1[0] == vs.GetSegPt1(activehandle)[0]:
+        endPt = vs.GetSegPt2(activehandle)
+
+    else:
+        endPt = vs.GetSegPt1(activehandle)
+    vs.MoveTo(endPt)
+    vs.LineTo(userPt2)
+    vs.AngularDim(upperPt, endPt, userPt2, 48, 771, 770, 0)
 
 
 _setVariables()
-vs.ForEachObject(_SetupPhotometrics, ("(SEL = True)"))
+vs.ForEachObject(lineHandles.append, "(SEL = True)")
+# Because Get Line cannot be used inside of a function and does not stop exectuion it is basically main()
+for handle in lineHandles:
+    activehandle = handle
+    vs.GetLine(_SavePts)
